2bb386a0869827df2b1159c2f597d9953a17c395
Pubudu Jayawardana <pubudusj@gmail.com>
2022-07-15 10:30:22 +0200

    Initial commit

---
+++ test/feature-flag-auto-off.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FeatureFlagAutoOff from '../lib/feature-flag-auto-off-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/feature-flag-auto-off-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FeatureFlagAutoOff.FeatureFlagAutoOffStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
